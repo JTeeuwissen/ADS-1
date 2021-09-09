787969ce4ef600353b667f842da311be242a737e
Jelle Teeuwissen <j.teeuwissen@picasse.com>
2021-09-09 14:49:13 +0200

    Added solution and projects

---
+++ VaccinationScheduling/VaccinationScheduling.Offline/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VaccinationScheduling.Offline
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ VaccinationScheduling/VaccinationScheduling.Shared/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace VaccinationScheduling.Shared
+{
+    public class Class1
+    {
+    }
+}
