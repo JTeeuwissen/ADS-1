dfd9902f5a504fc5cc7284a909ed55e47d1ab520
Jelle Teeuwissen <j.teeuwissen@picasse.com>
2021-09-14 08:54:54 +0200

    FFT: added schedule class

---
+++ VaccinationScheduling/VaccinationScheduling.Shared/Schedule.cs
@@ -0,0 +1,34 @@
+﻿namespace VaccinationScheduling.Shared
+{
+    public class Schedule
+    {
+        /// <summary>
+        /// Timeslot jab 1
+        /// </summary>
+        public int T1 { get; set; }
+
+        /// <summary>
+        /// Hospital jab 1
+        /// </summary>
+        public int M1 { get; set; }
+
+        /// <summary>
+        /// Timeslot jab 2
+        /// </summary>
+        public int T2 { get; set; }
+
+        /// <summary>
+        /// Hospital jab 2
+        /// </summary>
+        public int M2 { get; set; }
+
+        /// <summary>
+        /// Format the schedule to the desired output.
+        /// </summary>
+        /// <returns>A string version of the schedule.</returns>
+        public override string ToString()
+        {
+            return $"{T1}, {M1}, {T2}, {M2}";
+        }
+    }
+}
