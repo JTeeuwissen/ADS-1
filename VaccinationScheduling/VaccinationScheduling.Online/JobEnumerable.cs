cbcf1a8b7c38a1d13972c1be31599b7ef6d4cccd
Jelle Teeuwissen <j.teeuwissen@picasse.com>
2021-09-14 09:09:09 +0200

    FFT: job enumerable

---
+++ VaccinationScheduling/VaccinationScheduling.Online/JobEnumerable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using VaccinationScheduling.Shared;
+
+namespace VaccinationScheduling.Online
+{
+    public class JobEnumerable : IEnumerable<Job>
+    {
+        public IEnumerator<Job> GetEnumerator()
+        {
+            while (true)
+            {
+                Job job;
+                try
+                {
+                    job =  ReadUtils.ReadJob();
+                }
+                catch
+                {
+                    // There are no more patients
+                    break;
+                }
+                yield return job;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- VaccinationScheduling/VaccinationScheduling.Online/Program.cs
+++ VaccinationScheduling/VaccinationScheduling.Online/Program.cs
@@ -1,35 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using VaccinationScheduling.Shared;
+﻿using VaccinationScheduling.Shared;
 
 namespace VaccinationScheduling.Online
 {
     public class Program
     {
         private static Global _global;
-        private static List<Job> _jobs = new();
+        private static JobEnumerable _jobs;
 
         public static void Main()
         {
             _global = ReadUtils.ReadGlobal();
-
-            while (true)
-            {
-                try
-                {
-                    Job job = ReadUtils.ReadJob();
-                    _jobs.Add(job);
-                }
-                catch (Exception e)
-                {
-                    // There are no more patients
-                    Console.WriteLine(e);
-                    break;
-                }
-
-                // TODO Schedule
-            }
-
+            _jobs = new JobEnumerable();
+            
+            // TODO Schedule
             // TODO Output
         }
     }
