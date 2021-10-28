753b6a851914bca716f951b1ba1171eb56e63f4d
Jelle Teeuwissen <j.teeuwissen@picasse.com>
2021-10-28 11:44:37 +0200

    Fixed print

--- VaccinationScheduling/VaccinationScheduling.Online/Program.cs
+++ VaccinationScheduling/VaccinationScheduling.Online/Program.cs
@@ -16,7 +16,7 @@ namespace VaccinationScheduling.Online
             {
                 (int machine1, int machine2, int tFirstJab, int tSecondJab) = machines.FindGreedySpot(job);
                 machines.ScheduleJobs(machine1, machine2, tFirstJab, tSecondJab);
-                Console.WriteLine((machine1, machine2, tFirstJab, tSecondJab));
+                Console.WriteLine(new Schedule(machine1, machine2, tFirstJab, tSecondJab));
             }
 
             WriteDebugLine($"Number machines used:" + machines.NrMachines);
